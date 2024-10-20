b5bd297bd1cf6dfef045af5f1145d825aba2746b
Anatolii Charikov <pepsh6881@gmail.com>
2024-10-20 17:03:27 +0300

    Segments stuff were added.
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,18 +1,36 @@
+;;
 
-ORG     0x7C00              ; BIOS loads the bootloader into RAM at absolute address 0x7C00
-BITS    16                  ; use 16-bit arch
+SYMBOL		equ 'K'
+BIOS_INTERRUPT	equ 0x10
 
-SYMBOL          equ 'K'
-BIOS_INTERRUPT  equ 0x10
+ORG	0
+BITS	16		; use 16-bit arch
+
+_start:
+	jmp short start
+	nop
+
+times 33 db 0
 
 start:
-    mov ah, 0x0E            ; teletype output (convention)
-    mov al, SYMBOL
-    xor bx, bx              ; zero bl and bh registers
+	jmp 0x7c0:next
+next:
+	cli		; disable (clear) interrupts to prevent CPU from handling interrupts
+	mov ax, 0x7c0
+	mov ds, ax
+	mov es, ax
+	xor ax, ax
+	mov ss, ax
+	mov sp, 0x7c00
+	sti		; enable interrupts
 
-    int BIOS_INTERRUPT
+	mov ah, 0x0E	; teletype output (convention)
+	mov al, SYMBOL
+	xor bx, bx	; zero bl and bh registers
 
-    jmp $
+	int BIOS_INTERRUPT
+
+	jmp $
 
 times 510 - ($ - $$) db 0
 
