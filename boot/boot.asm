8cfb33e57d25ea51d3497ec814da2ee64bc07716
Anatolii Charikov <pepsh6881@gmail.com>
2024-10-21 23:09:50 +0300

    A few interrupts were added.
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- boot/boot.asm
+++ boot/boot.asm
@@ -6,6 +6,7 @@ BIOS_INTERRUPT	equ 0x10
 ORG	0
 BITS	16		; use 16-bit arch
 
+; BIOS Parameter  Block (BPB) stuff:
 _start:
 	jmp short start
 	nop
@@ -14,6 +15,21 @@ times 33 db 0
 
 start:
 	jmp 0x7c0:next
+
+handle_zero:
+	mov ah, 0x0e
+	mov al, '0'
+	xor bx, bx
+	int 0x10
+	iret
+
+handle_int_one:
+	mov ah, 0x0e
+	mov al, '1'
+	xor bx, bx
+	int 0x10
+	iret
+
 next:
 	cli		; disable (clear) interrupts to prevent CPU from handling interrupts
 	mov ax, 0x7c0
@@ -24,6 +40,14 @@ next:
 	mov sp, 0x7c00
 	sti		; enable interrupts
 
+	mov word[ss:0x00], handle_zero
+	mov word[ss:0x02], 0x7c0
+
+	mov word[ss:0x04], handle_int_one
+	mov word[ss:0x06], 0x7c0
+
+
+	int 1
 	mov ah, 0x0E	; teletype output (convention)
 	mov al, SYMBOL
 	xor bx, bx	; zero bl and bh registers
